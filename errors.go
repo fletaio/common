0b054e224a513c62cbf037014e422996adcf77c6
Lucas Park <indev@firstchain.co>
2018-09-16 16:37:44 +0900

    add tag

--- errors.go
+++ errors.go
@@ -7,6 +7,8 @@ import (
 var (
 	// ErrInvalidAddressFormat TODO
 	ErrInvalidAddressFormat = errors.New("invalid address format")
+	// ErrInvalidTagFormat TODO
+	ErrInvalidTagFormat = errors.New("invalid tag format")
 	// ErrInvalidSignature TODO
 	ErrInvalidSignature = errors.New("invalid signature")
 )
